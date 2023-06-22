2b2bec4520971f5e8859458c18f38e07c16eb3f5
E301 sede B Tecnologico Comfenalco <E301@Academico.local>
2023-06-21 21:24:54 -0500

    iniciar multiplicacion

--- holamundo.js
+++ holamundo.js
@@ -2,4 +2,13 @@ const express = require('express');
 const holamundo = express();
 holamundo.get('/', (req, res) =>
 res.send('Hola Mundo!'));
+
+holamundo.get('/multiplicar', (req,multi)=>{
+
+    let num1 = parseInt(req.query.num1);
+    let num2 = parseInt(req.query.num2);
+ 
+    multi.send("la multiplicación es: " + (num1*num2));
+});
+
 module.exports = holamundo;
