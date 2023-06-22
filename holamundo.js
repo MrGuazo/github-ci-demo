585ccdf3e45e1b4b8038fa3b1489fd027aad637b
Luis Felipe Guazo <luisfelipega18@gmail.com>
2023-06-21 21:10:58 -0500

    aplicando division

--- holamundo.js
+++ holamundo.js
@@ -2,4 +2,16 @@ const express = require('express');
 const holamundo = express();
 holamundo.get('/', (req, res) =>
 res.send('Hola Mundo!'));
+
+// Function to handle the root path
+holamundo.get('/division', async function(req, res) {
+
+    // Access the provided 'page' and 'limt' query parameters
+    let num1 = parseInt(req.query.num1);
+    let num2 = parseInt(req.query.num2);
+
+    res.send("La división es: "+ (num1 / num2));
+});
+
+
 module.exports = holamundo;
