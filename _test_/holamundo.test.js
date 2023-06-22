08efd92e843a7339ea05c3fd7577207ef0511ca6
Dafmau6 <137238831+Dafmau6@users.noreply.github.com>
2023-06-21 21:42:24 -0500

    Update holamundo.test.js

--- _test_/holamundo.test.js
+++ _test_/holamundo.test.js
@@ -7,3 +7,11 @@ describe('GET /', () => {
             .expect('Hola Mundo!', done);
     });
 });
+
+describe('GET /resta?num1=8&num2=9', () => {
+    it('responde con la resta es: -1', done => {
+        request(app)
+            .get('/resta?num1=8&num2=9')
+            .expect('la resta es: -1', done);
+    });
+});
