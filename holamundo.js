3dd503cc912d380ae0cb280c2867bbf6251f8721
Dafmau6 <137238831+Dafmau6@users.noreply.github.com>
2023-06-22 19:00:49 -0500

    Update holamundo.js

--- holamundo.js
+++ holamundo.js
@@ -3,14 +3,18 @@ const holamundo = express();
 holamundo.get('/', (req, res) =>
 res.send('Hola Mundo!'));
 
-holamundo.get('/multiplicar', (req, res) => {
 
-    let num1 = parseInt(req.query.num1);
+// Function to handle the root path
+holamundo.get('/resta', (req, res) => {
+
+    // Access the provided 'page' and 'limt' query parameters
+    let num1 = parseInt(req.query.num1); 
     let num2 = parseInt(req.query.num2);
- 
-    res.send("la multiplicación es: " + ( num1 * num2 ));
+        
+
+    // Return the articles to the rendering engine
+    res.send("la resta es: "+ (num1 - num2 ));
+
 });
 
-
 module.exports = holamundo;
-
