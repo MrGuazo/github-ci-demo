3278cf584d49678d9e98c8a74f774e670c5cc592
Luis Felipe Guazo <luisfelipega18@gmail.com>
2023-06-24 21:08:04 -0500

    aplicando feature/divisio

--- _test_/holamundo.test.js
+++ _test_/holamundo.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../holamundo');
+const app = require('../app');
 describe('GET /', () => {
     it('responde con Hola Mundo!', done => {
         request(app)
